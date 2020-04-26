d08d868c60cf970c7a429fe29213b79b98174ae1
yuanzhou <yuanzhou19@gmail.com>
2020-04-26 17:43:17 -0400

    Better function calls

--- src/app.py
+++ src/app.py
@@ -41,44 +41,43 @@ def index():
 # Both HTTP GET and HTTP POST can be used to execute search with body against ElasticSearch REST API. 
 @app.route('/search', methods = ['GET', 'POST'])
 def search():
-    access_group_open = "Open"
-
     # Always expect a json body
     if not request.is_json:
-        abort(400, description = 'This search request requries a json body')
+        bad_request('This search request requries a json body')
 
     # Parse incoming json string into json data(python dict object)
     json_data = request.get_json()
 
     # The query being sent in the body must be nested in a query key
     if "query" not in json_data:
-    	abort(400, description = 'The query being sent in the json body must be nested in a "query" key')
+    	bad_request('The query being sent in the json body must be nested in a "query" key')
 
     user_info = get_user_info_for_access_check(request, True)
 
     pprint("======user_info======")
     pprint(user_info)
 
-    modify_query = False
+    # Flag to indicate if to modify the JSON body or not
+    invalid_token = False
 
     # If returns error response, invalid header or token
     # Modify the search json and only search on the documents with `access_group` attribute's value as "Open"
     if isinstance(user_info, Response):
-        modify_query = True
+        invalid_token = True
     # Otherwise, user_info is a dict and we check user_info['hmgroupids'] list
     # Key 'hmgroupids' presents only when group_required is True
     else:
         if app.config['GLOBUS_HUBMAP_READ_GROUP_UUID'] not in user_info['hmgroupids']:
-            modify_query = True
+            invalid_token = True
 
-    pprint("======modify_query======")
-    pprint(modify_query)
+    pprint("======invalid_token======")
+    pprint(invalid_token)
     
-    # Modify the orgional json data if invalid authorization token provided 
+    # Modify the orgional "query" object if invalid authorization token provided 
     # or the user doesn't belong to the HuBMAP read group
     # Otherwise pass through the query as is
-    if modify_query:
-        modify_query(json_data)
+    if invalid_token:
+        modify_query(json_data["query"])
 
     pprint("=======Final JSON========")
     pprint(json_data)
@@ -131,33 +130,9 @@ def get_user_info_for_access_check(request, group_required):
 def bad_request(err_msg):
     abort(400, description = err_msg)
 
-# Convert the orginal leaf query into a compound query with modification
-def convert_leaf_to_compound(json_data, key, leaf_query_to_add):
-    # Add new property "bool" -> "must"
-    json_data["query"]["bool"] = {}
-    json_data["query"]["bool"]["must"] = []
-
-    # Move the original leaf query to the must list
-    orig_leaf_query = {
-        key: json_data["query"][key]
-    }
-
-    json_data["query"]["bool"]["must"].append(orig_leaf_query)
-    # Also add the access_group match restriction
-    json_data["query"]["bool"]["must"].append(leaf_query_to_add)
-
-    # And delete the leaf query from original query otherwise it's invalid format
-    del json_data["query"][key]
-
-    pprint (json_data)
-
-
-def modify_query(json_data):
-    # Error message if 'access_group' used in the orginal query
-    err_msg_denied_access_group_usage = "You can not use 'access_group' in request JSON"
-    
-    # Error message for unsupported query clauses
-    err_msg_unsupported_query_clause = "Sorry, the request JSON contains unsupported search query clause"
+# Modify the elasticsearch query JSON
+def modify_query(query_obj):
+    access_group_open = "Open"
 
     # Leaf query to be added for access open data only
     leaf_query_to_add = {
@@ -167,74 +142,88 @@ def modify_query(json_data):
     }
 
     # We'll first need to decide if the original query is a leaf query or compound query
-    # Case of compound query
-    if "bool" in json_data["query"]:
-        bool_query_obj = json_data["query"]["bool"]
+    # Leaf query being checked: match_all, match, match_phrase, term
+    # Compound query being checked: bool
 
-        # "must": The clause (query) must appear in matching documents and will contribute to the score
-        if "must" in bool_query_obj and "filter" not in bool_query_obj:
-            query_clause_list = bool_query_obj["must"]
+    # Case of compound query - bool query
+    if "bool" in query_obj:
+        modify_bool_query(query_obj["bool"], leaf_query_to_add)
+    # Case of leaf query - match_all
+    elif "match_all" in query_obj:
+        convert_leaf_to_compound(query_obj, "match_all", leaf_query_to_add)
+    # Case of leaf query - match
+    elif "match" in query_obj:
+        convert_leaf_to_compound(query_obj, "match", leaf_query_to_add)
+    # Case of leaf query - match_phrase
+    elif "match_phrase" in query_obj:
+        convert_leaf_to_compound(query_obj, "match_phrase", leaf_query_to_add)
+    # Another case of leaf query - term
+    elif "term" in query_obj:
+        convert_leaf_to_compound(query_obj, "term", leaf_query_to_add)
+    # Other unsupported queries regardless of leaf (e.g., range) or compound (e.g., dis_max)
+    else:
+        # Error message for unsupported query clauses
+        err_msg_unsupported_query_clause = "Sorry, the request JSON contains unsupported search query clause"
+        bad_request(err_msg_unsupported_query_clause)
 
-        # "filter": The clause (query) must appear in matching documents. However unlike "must" the score of the query will be ignored
-        if "filter" in bool_query_obj and "must" not in bool_query_obj:
-            query_clause_list = bool_query_obj["filter"]
+# Key: match_all, match, match_phrase, term
+def convert_leaf_to_compound(query_obj, key, leaf_query_to_add):
+    # Error message if 'access_group' used in the orginal query
+    err_msg_denied_access_group_usage = "You can not use 'access_group' in request JSON"
 
-        # When both "must" and "filter" clauses present, modify the "must" list
-        if "must" in bool_query_obj and "filter" in bool_query_obj:
-            query_clause_list = bool_query_obj["must"]
+    if 'access_group' in query_obj[key]:
+        bad_request(err_msg_denied_access_group_usage)
 
-        # When neither "must" nor "filter" clause presents, add an empty "must" list
-        if "must" not in bool_query_obj and "filter" not in bool_query_obj:
-            bool_query_obj["must"] = []
-            query_clause_list = bool_query_obj["must"]
+    # When check passes, convert the leaf query into a compound query with modification
+    # Convert the orginal leaf query into a compound query with modification
+    # Add new property "bool" -> "must"
+    query_obj["bool"] = {}
+    query_obj["bool"]["must"] = []
 
-        # If by any chance the request json contains `access_group`,
-        # we'll response 400 error for security concern
-        for obj in query_clause_list:
-            # The `access_group` field contains a single word at this moment, 
-            # so we'll cover all possible cases below
+    # Move the original leaf query to the must list
+    query_obj["bool"]["must"].append(query_obj[key])
+    # Also add the access_group match restriction
+    query_obj["bool"]["must"].append(leaf_query_to_add)
 
-            # Case 1: simple "match"
-            # Matches if one term is a match, doesn't care about the order of terms
-            if 'match' in obj and 'access_group' in obj['match']:
-                bad_request(err_msg_denied_access_group_usage)
+    # And delete the leaf query from original query otherwise it's invalid format
+    del query_obj[key]
 
-            # Case 2: "match_phrase"
-            # Matches only if the terms come in the same order
-            if 'match_phrase' in obj and 'access_group' in obj['match_phrase']:
-                bad_request(err_msg_denied_access_group_usage)
-        
+# Only modify the bool query object
+def modify_bool_query(bool_obj, leaf_query_to_add):
+    # "must": The clause (query) must appear in matching documents and will contribute to the score
+    # "filter": The clause (query) must appear in matching documents. However unlike "must" the score of the query will be ignored
+    # When "must" presents (regardless of "filter" presents), modify the "must" list
+    # Otherwise, mmodify "filter" clause if it presents or create an empty "must" if no "filter"
+    if "must" in bool_obj:
+        validate_query_clause_list(bool_obj["must"])
+
         # When the checks pass("access_group" is not used in the request)
         # we'll modify the orginal query with this simple leaf query(dict object)
-        query_clause_list.append(leaf_query_to_add)
-    
-    # Case of leaf query - match
-    elif "match" in json_data["query"]:
-        orig_leaf_query_obj = json_data["query"]["match"]
-        if 'access_group' in orig_leaf_query_obj:
-            bad_request(err_msg_denied_access_group_usage)
-
-        # When check passes, convert the leaf query into a compound query with modification
-        convert_leaf_to_compound(json_data, "match", leaf_query_to_add)
-
-    # Case of leaf query - match_phrase
-    elif "match_phrase" in json_data["query"]:
-        orig_leaf_query_obj = json_data["query"]["match_phrase"]
-        if 'access_group' in orig_leaf_query_obj:
-            bad_request(err_msg_denied_access_group_usage)
-
-        # When check passes, convert the leaf query into a compound query with modification
-        convert_leaf_to_compound(json_data, "match_phrase", leaf_query_to_add)
-
-    # Another case of leaf query - term
-    elif "term" in json_data["query"]:
-        orig_leaf_query_obj = json_data["query"]["term"]
-        if 'access_group' in orig_leaf_query_obj:
-            bad_request(err_msg_denied_access_group_usage)
-
-        # When check passes, convert the leaf query into a compound query with modification
-        convert_leaf_to_compound(json_data, "term", leaf_query_to_add)
-
-    # Other unsupported queries regardless of leaf or compound
+        bool_obj["must"].append(leaf_query_to_add)
     else:
-        bad_request(err_msg_unsupported_query_clause)
+        # Modify the "filter" clause if presents
+        if "filter" in bool_obj:
+            validate_query_clause_list(bool_obj["filter"])
+            bool_obj["filter"].append(leaf_query_to_add)
+        else:
+            # When neither "must" nor "filter" clause presents, add an empty "must" list
+            # And no validatation needed
+            bool_obj["must"] = []
+            bool_obj["must"].append(leaf_query_to_add)
+
+# If by any chance the request json contains `access_group`,
+# we'll response 400 error for security concern
+def validate_query_clause_list(query_clause_list):
+    for obj in query_clause_list:
+        # The `access_group` field contains a single word at this moment, 
+        # so we'll cover all possible cases below
+
+        # Case 1: simple "match"
+        # Matches if one term is a match, doesn't care about the order of terms
+        if 'match' in obj and 'access_group' in obj['match']:
+            bad_request(err_msg_denied_access_group_usage)
+
+        # Case 2: "match_phrase"
+        # Matches only if the terms come in the same order
+        if 'match_phrase' in obj and 'access_group' in obj['match_phrase']:
+            bad_request(err_msg_denied_access_group_usage)
