3df2f13177a059144f9ae38064a6a33d18959879
Zhou (Joe) Yuan <yuanzhou19@gmail.com>
2020-04-30 11:25:49 -0400

    Revert "Search query zhou"

--- README.md
+++ README.md
@@ -1,91 +1,33 @@
 # HuBMAP Search API
 
-The HuBMAP Search API is a thin wrapper of the Elasticsearch. It handles data indexing and reindexing into the backend Elasticsearch. It also accepts the search query and passes through to the Elasticsearch with data access security check.
+## Overview of tools
 
-## Search endpoint and group access check
+- [Docker Engine](https://docs.docker.com/install/)
+- [Docker Compose](https://docs.docker.com/compose/install/)
 
-The search endpoint for each deployment environment:
+Note: Docker Compose requires Docker to be installed and running first.
 
-- DEV: `https://search-api.dev.hubmapconsortium.org/search`
-- TEST: `https://search-api.test.hubmapconsortium.org/search`
+## Setup local development with Elasticsearch and Kibana with Docker Compose
 
-Both HTTP `GET` and `POST` are supported. Due to data access restriction, some indexed entries are protected and require the `Authorization` header with the Bearer token (globus nexus token) along with the search query JSON body. With a valid token (that represents a user who has group access to the indexed data), **ALL** the user specified search query DSL (Domain Specific Language) detail will be passed to the Elasticsearch just like making queries against the Elasticsearch directly, and the search hits results will be returned.
-
-If a token is missing or invalid, only public accessible data entries that have been indexed will be returned for the provided query. This is being done by modifying the orignal query JSON. As a result, not all queries defined by Elasticsearch are supported by this Search API. The following are the supported query clauses in this use case:
-
-- Leaf query clauses: `match_all`, `match`, `match_phrase`, `term`, `terms`, `terms_set`, `range`, `exists`, `ids`, `type`, `prefix`, `match_phrase_prefix`, `match_bool_prefix`, `fuzzy`, `wildcard`, `regexp`
-- Compound query clauses: `bool`, `dis_max`
-
-NOTE: currently, the Search API doesn't support comma-separated list or wildcard expression of index names used to limit the request.
-
-## Query examples
-
-The request JSON body to this Search API must start with "query" element
-
-````
-{
-    "query": {
-         
-    }
-    ...
-}
-````
-
-### Leaf query - match
-
-````
-{
-    "query": {
-        "match": {
-            "uuid": "4cac248a51b6767e029663b273e7a8b2"
-        }
-    }
-}
-````
-
-Note: 
-
-### Compound query - bool
-
-````
-{
-    "query": {
-        "bool": {
-            "must": [
-                {
-                    "match_phrase": {
-                        "donor.group_name": "Vanderbilt TMC"
-                    }
-                }
-            ],
-            "filter": [
-                {
-                    "match": {
-                        "origin_sample.entity_type": "Sample"
-                    }
-                }
-            ]
-        }
-    }
-}
-````
-
-For a request with a valid token that resprents a member who belongs to the HuBMAP read group, the request JSON may narrow down the hits with the `access_group` field, currently only "Open" and "Readonly" are the valid values.
-
-````
-{
-    "query": {
-        "term": {
-            "access_group": "Readonly"
-        }
-    }
-}
-````
-
-### With Python requests
+To start up the Elasticsearch and Kibana containers:
 
 ```
-query_dict = {
+cd docker
+sudo docker-compose up -d
+```
+## Usage Examples
+
+The search endpoint is
+````
+https://search-api.dev.hubmapconsortium.org/search
+````
+
+Both HTTP `GET` and `POST` are supported. It's optional to use the `Authorization` header with the Bearer token (globus nexus token). If the token represents a user who has group access to the indexed data, the search API will pass the query to the backend elasticsearch server and return the search hits that match the query defined in the request. If a token is not present or invalid, only data marked as public will be returned.
+
+### With Python Requests
+
+```
+query = {
     'query': {
         'match': {
             'uuid': uuid
@@ -94,15 +36,132 @@ query_dict = {
 }
 response = requests.post(
     'https://search-api.dev.hubmapconsortium.org/search',
-    json = query_dict,
-    headers = {'Authorization': 'Bearer ' + nexus_token})
+    json=query,
+    headers={'Authorization': 'Bearer ' + nexus_token})
 hits = response.json()['hits']['hits']
 ```
 
-Again, the `Authorization` is optional. Valid token in the header will allow all queries to be passed to the backend Elasticsearch. And missing or invalid token will only apply the search against the public accessible indexed data entries.
+### Longer JSON Examples
 
-## Deploy with other HuBMAP docker compose projects
+Below is the sample JSON in the request. 
 
-This option allows you to setup all the pieces in a containerized environment with docker and docker-compose. This requires to have the [HuBMAP Gateway](https://github.com/hubmapconsortium/gateway) running locally before starting building the Search API docker compose project. Please follow the [instructions](https://github.com/hubmapconsortium/gateway#workflow-of-setting-up-multiple-hubmap-docker-compose-projects). It also requires the Gateway project to be configured accordingly.
+````
+{
+  "version": true,
+  "size": 5000,
+  "sort": [
+    {
+      "_score": {
+        "order": "desc"
+      }
+    }
+  ],
+  "_source": {
+    "excludes": []
+  },
+  "stored_fields": [
+    "*"
+  ],
+  "script_fields": {},
+  "docvalue_fields": [],
+  "query": {
+    "bool": {
+      "must": [],
+      "filter": [
+        {
+          "match_all": {}
+        }
+      ],
+      "should": [],
+      "must_not": []
+    }
+  }
+}
+````
 
-For local development (the localhost mode), this Docker Compose project also comes with Elasticsearch and Kibana.
+You can also narrow down the search by adding a match phrase like this:
+
+````
+{
+  "version": true,
+  "size": 5000,
+  "sort": [
+    {
+      "_score": {
+        "order": "desc"
+      }
+    }
+  ],
+  "_source": {
+    "excludes": []
+  },
+  "stored_fields": [
+    "*"
+  ],
+  "script_fields": {},
+  "docvalue_fields": [],
+  "query": {
+    "bool": {
+      "must": [
+        {
+          "match_phrase": {
+            "display_doi": {
+              "query": "HBM762.FHCT.952"
+            }
+          }
+        }
+      ],
+      "filter": [
+        {
+          "match_all": {}
+        }
+      ],
+      "should": [],
+      "must_not": []
+    }
+  }
+}
+````
+
+For a request with a valid token that resprents a member who belongs to the HuBMAP read group, the request JSON may narrow down the search with the `access_group` field, currently only "Open" and "Readonly" are the valid values.
+
+````
+{
+  "version": true,
+  "size": 5000,
+  "sort": [
+    {
+      "_score": {
+        "order": "desc"
+      }
+    }
+  ],
+  "_source": {
+    "excludes": []
+  },
+  "stored_fields": [
+    "*"
+  ],
+  "script_fields": {},
+  "docvalue_fields": [],
+  "query": {
+    "bool": {
+      "must": [
+      {
+            "match_phrase": {
+                "access_group": {
+                    "query": "Open"
+                }
+            }
+        }],
+      "filter": [
+        {
+          "match_all": {}
+        }
+      ],
+      "should": [],
+      "must_not": []
+    }
+  }
+}
+````
--- src/app.py
+++ src/app.py
@@ -5,8 +5,6 @@ from flask import Flask, jsonify, abort, request, make_response, json, Response
 import threading
 import requests
 
-import json
-
 # HuBMAP commons
 from hubmap_commons.hm_auth import AuthHelper
 
@@ -41,57 +39,64 @@ def index():
 # Both HTTP GET and HTTP POST can be used to execute search with body against ElasticSearch REST API. 
 @app.route('/search', methods = ['GET', 'POST'])
 def search():
+    access_group_open = "Open"
+
     # Always expect a json body
     if not request.is_json:
-        bad_request("This search request requries a JSON body")
+        abort(400, description = 'This search request requries a json body')
 
     # Parse incoming json string into json data(python dict object)
     json_data = request.get_json()
-
-    # The query being sent in the body must be nested in a query key
-    if "query" not in json_data:
-        bad_request("The query being sent in the JSON body must be nested in a 'query' key")
-
-    # Only one key is allowed in the query outer level - query
-    if len(json_data['query']) > 1:
-        bad_request("The 'query' context in the JSON body should only contain one top-level key")
 
     user_info = get_user_info_for_access_check(request, True)
 
     pprint("======user_info======")
     pprint(user_info)
 
-    # Flag to indicate if to modify the JSON body or not
-    invalid_token = False
+    modify_query = False
 
     # If returns error response, invalid header or token
     # Modify the search json and only search on the documents with `access_group` attribute's value as "Open"
     if isinstance(user_info, Response):
-        invalid_token = True
+        modify_query = True
     # Otherwise, user_info is a dict and we check user_info['hmgroupids'] list
     # Key 'hmgroupids' presents only when group_required is True
     else:
         if app.config['GLOBUS_HUBMAP_READ_GROUP_UUID'] not in user_info['hmgroupids']:
-            invalid_token = True
+            modify_query = True
 
-    pprint("======invalid_token======")
-    pprint(invalid_token)
+    pprint("======modify_query======")
+    pprint(modify_query)
     
-    # Modify the orginal "query" object if invalid or no authorization token provided 
+    # Modify the orgional json data if invalid authorization token provided 
     # or the user doesn't belong to the HuBMAP read group
     # Otherwise pass through the query as is
-    if invalid_token:
-        modify_query(json_data["query"])
+    if modify_query:
+        query_must_list = json_data["query"]["bool"]["must"]
+        
+        # If by any chance the request json contains match phrase with `access_group`,
+        # we'll response 400 error for security concern
+        for obj in query_must_list:
+        	if 'match_phrase' in obj and 'access_group' in obj['match_phrase']:
+        		abort(400, description = "You can not use 'access_group' in request JSON")
 
-    pprint("=======Final json_data (dict)========")
-    pprint(json_data)
+        # dict object
+        query_to_add = {
+            "match_phrase": {
+                "access_group": {
+                    "query": access_group_open
+                }
+            }
+        }
+
+        # Modify the request json when "access_group" is not used in the request
+        query_must_list.append(query_to_add)
 
     # When the user belongs to the HuBMAP read group,
     # simply pass the search json to elasticsearch
     # The request json may contain "access_group" in this case
     target_url = app.config['ELASTICSEARCH_URL'] + '/' + '_search'
-    # Make a request with json data
-    # The use of json parameter converts python dict to json string and adds content-type: application/json automatically
+    # Make a request with json data (adds content-type: application/json automatically)
     resp = requests.post(url = target_url, json = json_data)
 
     # return the elasticsearch resulting json data as json string
@@ -130,133 +135,4 @@ def init_auth_helper():
 def get_user_info_for_access_check(request, group_required):
     auth_helper = init_auth_helper()
     return auth_helper.getUserInfoUsingRequest(request, group_required)
-
-# Throws error for bad reqeust with message
-def bad_request(err_msg):
-    abort(400, description = err_msg)
-
-# Modify the elasticsearch query JSON
-def modify_query(query_dict):
-    access_group_open = "Open"
-
-    # Leaf query (python dict) to be added for access open data only
-    leaf_query_dict_to_add = {
-        "match": {
-            "access_group": access_group_open
-        }
-    }
-
-    # We'll first need to decide if the original query is a leaf query or compound query
-    supported_leaf_query_keys = get_supported_query_keys("leaf")
-    supported_compound_query_keys = get_supported_query_keys("compound")
-
-    # The query context dict contains only one key
-    query_key = list(query_dict)[0]
-    pprint("query key: " + query_key)
-
-    # Leaf query
-    if query_key in supported_leaf_query_keys:
-        convert_leaf_to_compound(query_dict, query_key, leaf_query_dict_to_add)
-    # Compound query
-    elif query_key in supported_compound_query_keys:
-        modify_compound_query(query_dict, query_key, leaf_query_dict_to_add)
-    # Other unsupported queries
-    # Regardless of leaf (e.g., match_none) or compound (e.g., boosting query, function_score query)
-    else:
-        bad_request("Sorry, this Search API doesn't support the provided search query: '" + query_key + "'")
-
-# Key: match_all, match, match_phrase, term
-def convert_leaf_to_compound(query_dict, key, leaf_query_dict_to_add):
-    # First make sure 'access_group' is not used
-    validate_access_group_usage(query_dict[key])
-
-    # When check passes, convert the leaf query into a compound query with modification
-    # Convert the orginal leaf query into a compound query with modification
-    # Add new property "bool" (dict) -> "must" (list)
-    query_dict["bool"] = {}
-    query_dict["bool"]["must"] = []
-
-    # Copy the original leaf query to the must list
-    orig_leaf_query = {
-        key: query_dict[key]
-    }
-
-    query_dict["bool"]["must"].append(orig_leaf_query)
-
-    # Also add the access_group match restriction
-    query_dict["bool"]["must"].append(leaf_query_dict_to_add)
-
-    # And delete the original leaf query otherwise it's invalid format
-    del query_dict[key]
-
-# Modify the compund query based on specific query key
-def modify_compound_query(query_dict, key, leaf_query_dict_to_add):
-    if key == 'bool':
-        modify_bool_query(query_dict[key], leaf_query_dict_to_add)
-
-    if key == 'dis_max':
-        modify_dis_max_query(query_dict[key], leaf_query_dict_to_add)
-
-# Only modify the bool query object (python dict)
-def modify_bool_query(bool_dict, leaf_query_dict_to_add):
-    # "must": The clause (query) must appear in matching documents and will contribute to the score
-    # "filter": The clause (query) must appear in matching documents. However unlike "must" the score of the query will be ignored
-    # When "must" presents (regardless of "filter" presents), modify the "must" list
-    # Otherwise, mmodify "filter" clause if it presents or create an empty "must" if no "filter"
-    # Both bool_dict["must"] and bool_dict["filter"] are list (they can be dict, we use list here)
-    if "must" in bool_dict:
-        validate_compound_query_clause_list(bool_dict["must"])
-
-        # When the checks pass("access_group" is not used in the request)
-        # we'll modify the orginal query with this simple leaf query(dict object)
-        bool_dict["must"].append(leaf_query_dict_to_add)
-    else:
-        # Modify the "filter" clause if presents
-        if "filter" in bool_dict:
-            validate_compound_query_clause_list(bool_dict["filter"])
-            bool_dict["filter"].append(leaf_query_dict_to_add)
-        else:
-            # When neither "must" nor "filter" clause presents, add an empty "must" list
-            # And no validatation needed
-            bool_dict["must"] = []
-            bool_dict["must"].append(leaf_query_dict_to_add)
-
-# Disjunction max query
-def modify_dis_max_query(dis_max_dict, leaf_query_dict_to_add):
-    # "queries" key is required, it's a list that contains one or more query clauses
-    if "queries" in dis_max_dict:
-        validate_compound_query_clause_list(dis_max_dict["queries"])
-
-        # When the checks pass("access_group" is not used in the request)
-        # we'll modify the orginal query with this simple leaf query(dict object)
-        dis_max_dict["queries"].append(leaf_query_dict_to_add)
-    else:
-        bad_request("'queries' is required top-level parameter in 'dis_max' query in request JSON body")
-
-def validate_compound_query_clause_list(query_clause_list):
-    for item in query_clause_list:
-        for query in get_supported_query_keys("leaf"):
-            if query in item:
-                validate_access_group_usage(item[query])
-        
-# Possible TO-DO: check against all nested keys?
-# If by any chance the request json contains `access_group` key in the top level,
-# we'll response 400 error for security concern
-# The `access_group` field contains a single word at this moment
-def validate_access_group_usage(dict):
-    # Error message if 'access_group' used in the orginal query
-    if 'access_group' in dict:
-        bad_request("You can not use 'access_group' in request JSON body")
-
-# Leaf and compound query keys shared with other function calls
-def get_supported_query_keys(key):
-    keys_dict = {
-        "leaf": ['match_all', 'match', 'match_phrase', 'multi_match', 'term', 'terms', 'terms_set', 'range', 'exists', 'ids', 'type', 'prefix', 'match_phrase_prefix', 'match_bool_prefix', 'fuzzy', 'wildcard', 'regexp'],
-        "compound": ['bool', 'dis_max']
-    }
-
-    try:
-        return keys_dict[key]
-    except KeyError:
-        print("Unknown key: '" + key + "' was used when calling get_supported_query_keys()")
 
