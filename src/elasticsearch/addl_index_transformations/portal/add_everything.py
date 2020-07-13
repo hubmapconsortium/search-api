3eeb3a703af112bbdc9975fca93dc5e84ad52096
mccalluc <mccalluc@users.noreply.github.com>
2020-07-13 12:33:28 -0400

    Ensure dynamic mapping is string: Fix #88

--- src/elasticsearch/addl_index_transformations/portal/add_everything.py
+++ src/elasticsearch/addl_index_transformations/portal/add_everything.py
@@ -8,19 +8,26 @@ def add_everything(doc):
     >>> doc = {
     ...   'a': {
     ...     'b': {
-    ...       'c': ['deep', 'deep']
+    ...       'c': ['', 'deep', 'deep']
     ...     }
     ...   }
     ... }
     >>> add_everything(doc)
     >>> doc
-    {'a': {'b': {'c': ['deep', 'deep']}}, 'everything': ['deep']}
+    {'a': {'b': {'c': ['', 'deep', 'deep']}}, 'everything': ['ensure_dynamic_mapping_is_string', 'deep']}
 
     '''
     everything = set(_get_nested_values(doc))
     # Sort for stability in tests;
     # Could be removed if it's a performance hit.
-    doc['everything'] = sorted(everything)
+
+    # With Elasticsearch's dynamic mapping, if the first string it sees in a field is a date,
+    # it will try to coerce successive values to dates, and this may fail.
+    # https://www.elastic.co/guide/en/elasticsearch/guide/2.x/mapping-intro.html#core-fields
+    # To get around this, this value makes sure the dynamic map will be a string.
+    # The real fix is explicit mappings, but that won't be available for a while. (ever?)
+    # https://github.com/hubmapconsortium/search-api/issues/63
+    doc['everything'] = ['ensure_dynamic_mapping_is_string'] + sorted(everything)
 
 
 def _get_nested_values(input):
@@ -28,7 +35,7 @@ def _get_nested_values(input):
     >>> doc = {
     ...   'a': {
     ...     'b0': {
-    ...       'c': ['deep', 'deep!']
+    ...       'c': ['', 'deep', 'deep!']
     ...     },
     ...     'b1': 'deep'
     ...   },
@@ -45,5 +52,5 @@ def _get_nested_values(input):
     elif isinstance(input, list):
         for value in input:
             yield from _get_nested_values(value)
-    else:
+    elif input:
         yield str(input)
