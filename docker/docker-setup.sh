8288b1681264196b36038e70de68d76cad3e22a8
yuanzhou <yuanzhou19@gmail.com>
2020-08-18 10:40:29 -0400

    Fixed path error

--- docker/docker-setup.sh
+++ docker/docker-setup.sh
@@ -3,7 +3,7 @@
 # Set the version environment variable for the docker build
 # Version number is from the VERSION file
 # Also remove newlines and leading/trailing slashes if present in that VERSION file
-export SEARCH_API_VERSION=$(tr -d "\n\r" < VERSION | xargs)
+export SEARCH_API_VERSION=$(tr -d "\n\r" < ../VERSION | xargs)
 
 echo "SEARCH_API_VERSION: $SEARCH_API_VERSION"
 
