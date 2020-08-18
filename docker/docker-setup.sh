91acbe4ef61befc138ee7eabe365d8c0fb93a317
yuanzhou <yuanzhou19@gmail.com>
2020-08-18 10:32:21 -0400

    Remove newlines and spaces from VERSION parse

--- docker/docker-setup.sh
+++ docker/docker-setup.sh
@@ -2,7 +2,8 @@
 
 # Set the version environment variable for the docker build
 # Version number is from the VERSION file
-export SEARCH_API_VERSION=`cat ../VERSION`
+# Also remove newlines and leading/trailing slashes if present in that VERSION file
+export SEARCH_API_VERSION=$(tr -d "\n\r" < VERSION | xargs)
 
 echo "SEARCH_API_VERSION: $SEARCH_API_VERSION"
 
