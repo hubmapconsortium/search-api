f69a3bc7767e04d9294684ed0892f96865c18d01
yuanzhou <yuanzhou19@gmail.com>
2020-08-17 21:23:22 -0400

    Added VERSION for docker image tag

--- docker/docker-setup.sh
+++ docker/docker-setup.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+# Set the version environment variable for the docker build
+# Version number is from the VERSION file
+export SEARCH_API_VERSION=`cat VERSION`
+
+echo "SEARCH_API_VERSION: $SEARCH_API_VERSION"
+
 # Copy over the src folder
 cp -r ../src search-api/
 
