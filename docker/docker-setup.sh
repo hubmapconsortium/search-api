0f8544ff448b8997e3125911cab76a591d4681e7
yuanzhou <yuanzhou19@gmail.com>
2020-08-18 23:12:00 -0400

    Added localhost build

--- docker/docker-setup.sh
+++ docker/docker-setup.sh
@@ -4,7 +4,6 @@
 # Version number is from the VERSION file
 # Also remove newlines and leading/trailing slashes if present in that VERSION file
 export SEARCH_API_VERSION=$(tr -d "\n\r" < ../VERSION | xargs)
-
 echo "SEARCH_API_VERSION: $SEARCH_API_VERSION"
 
 # Copy over the src folder
