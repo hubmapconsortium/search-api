8e393c88ec9da09f9e398fbcfffb99a39a79d2f0
yuanzhou <yuanzhou19@gmail.com>
2020-08-20 22:56:09 -0400

    Banner, src copy, BUILD with git branch:commit short hash

--- docker/search-api-docker.sh
+++ docker/search-api-docker.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
+# Print a new line and the banner
+echo
+echo "==================== SEARCH-API ===================="
+
+# The `absent_or_newer` checks if the copied src at docker/some-api/src directory exists 
+# and if the source src directory is newer. 
+# If both conditions are true `absent_or_newer` writes an error message 
+# and causes script to exit with an error code.
 function absent_or_newer() {
     if  [ \( -e $1 \) -a \( $2 -nt $1 \) ]; then
         echo "$1 is out of date"
@@ -19,13 +27,14 @@ function get_dir_of_this_script() {
     DIR="$( cd -P "$( dirname "$SCRIPT_SOURCE" )" >/dev/null 2>&1 && pwd )"
 }
 
-# Generate the build version based on git commit Id and store into BUILD file
+# Generate the build version based on git branch name and short commit hash and write into BUILD file
 function generate_build_version() {
-    GIT_COMMIT_ID=$(git rev-parse --verify HEAD)
-    echo "BUILD version for Elasticsearch mapper_metadata.version: $GIT_COMMIT_ID"
+    GIT_BRANCH_NAME=$(git branch | sed -n -e 's/^\* \(.*\)/\1/p')
+    GIT_SHORT_COMMIT_HASH=$(git rev-parse --short HEAD)
+    echo "BUILD version for Elasticsearch mapper_metadata.version: $GIT_BRANCH_NAME:$GIT_SHORT_COMMIT_HASH"
     # Clear the old BUILD version and write the new one
     truncate -s 0 ../BUILD
-    echo $GIT_COMMIT_ID >> ../BUILD
+    echo $GIT_BRANCH_NAME:$GIT_SHORT_COMMIT_HASH >> ../BUILD
 }
 
 # Set the version environment variable for the docker build
@@ -58,17 +67,18 @@ else
                 fi
             done
 
-            # The `absent_or_newer` checks if the copied src at docker/some-api/src directory exists 
-            # and if the source src directory is newer. 
-            # If both conditions are true `absent_or_newer` writes an error message 
-            # and causes hubmap-docker.sh to exit with an error code.
             absent_or_newer search-api/src ../src
 
             echo 'Checks complete, all good :)'
         elif [ "$2" = "config" ]; then
             export_version
-            docker-compose -p search-api -f docker-compose.yml -f docker-compose.$1.yml config
+            docker-compose -f docker-compose.yml -f docker-compose.$1.yml -p search-api config
         elif [ "$2" = "build" ]; then
+            # Delete the copied source code dir if exists
+            if [ -d "search-api/src" ]; then
+                rm -rf search-api/src
+            fi
+
             # Copy over the src folder
             cp -r ../src search-api/
 
@@ -76,16 +86,16 @@ else
             generate_build_version
             
             export_version
-            docker-compose -f docker-compose.yml -f docker-compose.$1.yml -p search-api --verbose build
+            docker-compose -f docker-compose.yml -f docker-compose.$1.yml -p search-api build
         elif [ "$2" = "start" ]; then
             export_version
-            docker-compose -f docker-compose.yml -f docker-compose.$1.yml -p search-api --verbose up -d
+            docker-compose -f docker-compose.yml -f docker-compose.$1.yml -p search-api up -d
         elif [ "$2" = "stop" ]; then
             export_version
-            docker-compose -f docker-compose.yml -f docker-compose.$1.yml -p search-api --verbose stop
+            docker-compose -f docker-compose.yml -f docker-compose.$1.yml -p search-api stop
         elif [ "$2" = "down" ]; then
             export_version
-            docker-compose -f docker-compose.yml -f docker-compose.$1.yml -p search-api --verbose down
+            docker-compose -f docker-compose.yml -f docker-compose.$1.yml -p search-api down
         fi
     fi
 fi
