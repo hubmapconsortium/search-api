63c7dcff14c6cc75136ecdfe8cd13fc1cf344d88
mccalluc <mccalluc@users.noreply.github.com>
2020-09-11 14:22:14 -0400

    warn -> debug

--- src/elasticsearch/addl_index_transformations/portal/__init__.py
+++ src/elasticsearch/addl_index_transformations/portal/__init__.py
@@ -130,15 +130,15 @@ def _map(doc, clean):
     for single_doc_field in single_valued_fields:
         if single_doc_field in doc:
             fragment = doc[single_doc_field]
-            logging.warning(f'Mapping "{single_doc_field}" fragment: {dumps(fragment)[:50]}...')
+            logging.debug(f'Mapping single "{single_doc_field}": {dumps(fragment)[:50]}...')
             _map(fragment, clean)
-            logging.warning(f'... done mapping "{single_doc_field}"')
+            logging.debug(f'... done mapping "{single_doc_field}"')
     for multi_doc_field in multi_valued_fields:
         if multi_doc_field in doc:
             for fragment in doc[multi_doc_field]:
-                logging.warning(f'Mapping "{multi_doc_field}" fragment: {dumps(fragment)[:50]}...')
+                logging.debug(f'Mapping multi "{multi_doc_field}": {dumps(fragment)[:50]}...')
                 _map(fragment, clean)
-                logging.warning(f'... done mapping "{multi_doc_field}"')
+                logging.debug(f'... done mapping "{multi_doc_field}"')
 
 
 def _simple_clean(doc):
--- src/elasticsearch/debug.py
+++ src/elasticsearch/debug.py
@@ -2,6 +2,7 @@
 
 import sys
 from pathlib import Path
+import logging
 
 from yaml import dump as dump_yaml, safe_load as load_yaml
 
@@ -11,8 +12,11 @@ from elasticsearch.addl_index_transformations.portal import transform
 if __name__ == "__main__":
     paths = sys.argv[1:]
     if len(paths) == 0:
-        raise Exception('Provide paths to JSON or YAML files as arguments')
+        print('Provide paths to JSON or YAML files as arguments')
+        sys.exit(1)
+    logging.basicConfig(level=logging.DEBUG)
     for path in paths:
         doc = load_yaml(Path(path).read_text())
-        transformed = transform(doc)
-        print(dump_yaml(transformed))
+        new_name = f'{path}.transformed.yaml'
+        Path(new_name).open('w').write(dump_yaml(transform(doc)))
+        print(f'Wrote {new_name}')
