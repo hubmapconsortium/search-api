efe5c2dde03910c6fc0e46417533e951bce3a741
Zhou (Joe) Yuan <yuanzhou19@gmail.com>
2020-09-15 22:47:48 -0400

    Delete old VERSION/BUILD file before build copy

--- docker/search-api-docker.sh
+++ docker/search-api-docker.sh
@@ -98,6 +98,16 @@ else
             # Only mount the VERSION file and BUILD file for localhost and dev
             # On test/stage/prod, copy the VERSION file and BUILD file to image
             if [[ "$1" != "localhost" && "$1" != "dev" ]]; then
+                # Delete old VERSION and BUILD files if found
+                if [ -f "search-api/src/VERSION" ]; then
+                    rm -rf search-api/src/VERSION
+                fi
+                
+                if [ -f "search-api/src/BUILD" ]; then
+                    rm -rf search-api/src/BUILD
+                fi
+                
+                # Copy over the one files
                 cp VERSION search-api/src
                 cp BUILD search-api/src
             fi
