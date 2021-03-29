7e05df3ad4760a725a619ab9b6359365f6f2d616
mccalluc <mccalluc@users.noreply.github.com>
2021-03-29 12:21:55 -0400

    Expand assertions against live ES

--- src/elasticsearch/addl_index_transformations/portal/test-es.py
+++ src/elasticsearch/addl_index_transformations/portal/test-es.py
@@ -1,11 +1,35 @@
 #!/usr/bin/env python
 
-import requests
 from json import loads
+from pathlib import Path
+
+import requests
+from yaml import safe_load as load_yaml
+
 
 base_url = 'http://127.0.0.1:9200'
 
-base_response = requests.get('http://127.0.0.1:9200').json()
+base_response = requests.get(base_url).json()
 assert 'cluster_name' in base_response
 
+index = 'test_index'
+delete_response = requests.delete(f'{base_url}/{index}').json()
+print(delete_response)
+assert 'error' in delete_response or delete_response['acknowledged']
+
+config = load_yaml((Path(__file__).parent / 'config.yaml').read_text())
+put_index_response = requests.put(f'{base_url}/{index}').json()
+print(put_index_response)
+assert put_index_response['acknowledged']
+
+put_doc_response = requests.put(f'{base_url}/{index}/_doc/1', json={'name': 'XYZ'}).json()
+print(put_doc_response)
+assert '_index' in put_doc_response
+
+get_doc_response = requests.get(f'{base_url}/{index}/_doc/1').json()
+print(get_doc_response)
+assert get_doc_response['_source']['name'] == 'XYZ'
+
 # TODO: Confirm that indexing works as expected.
+
+print('No errors!')
