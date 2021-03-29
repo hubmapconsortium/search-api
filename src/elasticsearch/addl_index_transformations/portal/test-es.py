b074b7d78350f72ae9fc696a52d0f121f619d478
mccalluc <mccalluc@users.noreply.github.com>
2021-03-29 16:20:49 -0400

    Needed "?refresh" to force synchronous response

--- src/elasticsearch/addl_index_transformations/portal/test-es.py
+++ src/elasticsearch/addl_index_transformations/portal/test-es.py
@@ -20,14 +20,24 @@ print(delete_response)
 assert 'error' in delete_response or delete_response['acknowledged']
 
 config = load_yaml((Path(__file__).parent / 'config.yaml').read_text())
-put_index_response = requests.put(f'{base_url}/{index}').json()
+put_index_response = requests.put(f'{base_url}/{index}', headers={'Content-Type': 'application/json'}, json=config).json()
 print(put_index_response)
 assert put_index_response['acknowledged']
 
+get_index_response = requests.get(f'{base_url}/{index}').json()
+print(get_index_response)
+assert 'full_name' in get_index_response[index]['mappings']['properties']
+
 # Add a document:
 
-doc = {'new_unexpected_field': 'XYZ'}
-put_doc_response = requests.put(f'{base_url}/{index}/_doc/1', json=doc).json()
+doc = {
+    "first_name": "John",
+    "last_name": "Smith"
+}
+# NOTE: Without "?refresh", the index is not guaranteed to be
+# up-to-date when the response returns. Should not be unused
+# in production, but necessary for a synchronous test like this.
+put_doc_response = requests.put(f'{base_url}/{index}/_doc/1?refresh', json=doc).json()
 print(put_doc_response)
 assert '_index' in put_doc_response
 
@@ -35,17 +45,20 @@ assert '_index' in put_doc_response
 
 get_doc_response = requests.get(f'{base_url}/{index}/_doc/1').json()
 print(get_doc_response)
-assert get_doc_response['_source']['new_unexpected_field'] == 'XYZ'
+assert get_doc_response['_source']['first_name'] == 'John'
 
-query = {'query': {'match': {'all_text': {
-  'query': 'XYZ',
-  'operator': 'and'
+query = {'query': {'match': {'full_name': {
+    "query": "John Smith",
+    "operator": "and"
 }}}}
 headers = {'Content-Type': 'application/json'}
-get_search_response = requests.get(f'{base_url}/{index}/_search', headers=headers, json=query).json()
+get_search_response = requests.request(
+  'GET',
+  url=f'{base_url}/{index}/_search',
+  headers=headers,
+  json=query
+).json()
 print(get_search_response)
-assert get_search_response == 'foo'
-
-# TODO: Confirm that indexing works as expected.
+assert len(get_search_response['hits']['hits']) == 1
 
 print('No errors!')
