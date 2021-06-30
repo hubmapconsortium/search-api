3c2db9b9e64f8ec94cc00ec0e939ef3abb0d91c5
mccalluc <mccalluc@users.noreply.github.com>
2021-06-29 22:28:32 -0400

    Exactly the report I want

--- etc/flat_fields.py
+++ etc/flat_fields.py
@@ -4,6 +4,7 @@ import argparse
 from pathlib import Path
 import json
 import sys
+import re
 
 from flatten_json import flatten
 
@@ -11,21 +12,24 @@ from flatten_json import flatten
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument(
-        '--json',
-        type=Path,
-        required=True,
+        '--json', type=Path, required=True,
         help='Nested JSON from ES that lists the fields in an index')
     parser.add_argument(
-        '--only',
-        type=str,
+        '--only', type=str,
         default='hm_public_portal.mappings._doc.properties.',
         help='Only print lines that start with this')
     parser.add_argument(
-        '--exclude',
-        type=str,
-        nargs='+',
+        '--exclude', type=str, nargs='+',
         default=['.fields.keyword.type', '.copy_to.0'],
         help='Exclude lines that end with these')
+    parser.add_argument(
+        '--dot', type=str,
+        default='.properties.',
+        help='Collapse to a single period')
+    parser.add_argument(
+        '--strip', type=str,
+        default='.type',
+        help='Suffix to strip')
     args = parser.parse_args()
     nested = json.load(args.json.open())
     for full_path, v in flatten(nested, '.').items():
@@ -33,7 +37,11 @@ def main():
             continue
         if any(full_path.endswith(suffix) for suffix in args.exclude):
             continue
-        short_path = full_path.replace(args.only, '', 1)
+        short_path = re.sub(
+            re.escape(args.strip) + '$',
+            '',
+            full_path
+        ).replace(args.only, '', 1).replace(args.dot, '.')
         print(f'{short_path}\t{v}')
     return 0
 
