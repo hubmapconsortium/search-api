adc22d66203e8899b2f25af13c309f1fe8a3c3ca
yuanzhou <yuanzhou19@gmail.com>
2022-05-27 11:34:01 -0400

    Add travis CI tests

---
+++ test.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+set -o errexit
+
+./generate-build-version.sh
+src/hubmap_translation/addl_index_transformations/portal/test.sh
+src/search-schema/test.sh
+src/search-adaptor/tree/test-release/src/libs/test.sh
