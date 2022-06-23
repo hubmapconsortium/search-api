c12a43f331d725ef7b4605fdc78072a09d8d5806
maxsibilla <maxsibilla@icloud.com>
2022-06-23 10:35:58 -0400

    Updating pathing for correct test script

--- test.sh
+++ test.sh
@@ -4,4 +4,4 @@ set -o errexit
 ./generate-build-version.sh
 src/hubmap_translation/addl_index_transformations/portal/test.sh
 src/search-schema/test.sh
-src/search-adaptor/src/libs/test.sh
+src/libs/test.sh
