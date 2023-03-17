af672bb6b9ee83c3affa56719ea6bc097ab419e7
yuanzhou <yuanzhou19@gmail.com>
2023-03-17 16:22:23 -0400

    Add missing import

--- src/hubmap_translator.py
+++ src/hubmap_translator.py
@@ -8,6 +8,7 @@ import re
 import sys
 import time
 from yaml import safe_load
+from datetime import datetime
 
 # For reusing the app.cfg configuration when running indexer_base.py as script
 from flask import Flask, Response
