e5c0a3e1f201cb7af1b33bc76ab739843449a7be
John Conroy <john-conroy@users.noreply.github.com>
2023-12-15 14:03:59 -0500

    Handle empty response from soft assay endpoint

--- src/hubmap_translation/addl_index_transformations/portal/add_assay_details.py
+++ src/hubmap_translation/addl_index_transformations/portal/add_assay_details.py
@@ -6,9 +6,11 @@ logging.basicConfig(format='[%(asctime)s] %(levelname)s in %(module)s: %(message
 logger = logging.getLogger(__name__)
 
 
-def _get_assay_details(uuid, transformation_resources):
+def _get_assay_details(doc, transformation_resources):
     soft_assay_url = transformation_resources.get('ingest_api_soft_assay_url')
     token = transformation_resources.get('token')
+    uuid = doc.get('uuid')
+    dataset_type = doc.get('dataset_type')
     try:
         response = requests.get(f'{soft_assay_url}/{uuid}', headers={'Authorization': f'Bearer {token}'})
         response.raise_for_status()
@@ -16,12 +18,14 @@ def _get_assay_details(uuid, transformation_resources):
         logger.error(e.response.text)
         raise
     json = response.json()
+    if not json:
+        return {'description' : dataset_type, 'vitessce-hints': []}
     return json
 
 
 def add_assay_details(doc, transformation_resources):
     if 'dataset_type' in doc:
-        assay_details = _get_assay_details(doc['uuid'], transformation_resources)
+        assay_details = _get_assay_details(doc, transformation_resources)
         # Preserve the previous shape of assay_types.
         doc['description'] = assay_details.get('description')
         doc['vitessce-hints'] = assay_details.get('vitessce-hints')
