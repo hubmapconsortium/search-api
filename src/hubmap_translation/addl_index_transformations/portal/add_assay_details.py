8f93bce1230c5ac697cce9d0ea816a02353ab295
John Conroy <john-conroy@users.noreply.github.com>
2023-12-19 15:59:33 -0500

    Get has_visualization using soft assay

--- src/hubmap_translation/addl_index_transformations/portal/add_assay_details.py
+++ src/hubmap_translation/addl_index_transformations/portal/add_assay_details.py
@@ -1,19 +1,18 @@
 import requests
 import logging
 
+from portal_visualization.builder_factory import has_visualization
+
 logging.basicConfig(format='[%(asctime)s] %(levelname)s in %(module)s: %(message)s', level=logging.DEBUG,
                     datefmt='%Y-%m-%d %H:%M:%S')
 logger = logging.getLogger(__name__)
 
 
-def _get_assay_details(doc, transformation_resources):
-    soft_assay_url = transformation_resources.get('ingest_api_soft_assay_url')
-    token = transformation_resources.get('token')
-    uuid = doc.get('uuid')
+def _get_assay_details(doc, uuid, request_url, headers):
     dataset_type = doc.get('dataset_type')
+
     try:
-        response = requests.get(
-            f'{soft_assay_url}/{uuid}', headers={'Authorization': f'Bearer {token}'})
+        response = requests.get(request_url, headers=headers)
         response.raise_for_status()
     except requests.exceptions.HTTPError as e:
         logger.error(e.response.text)
@@ -27,7 +26,23 @@ def _get_assay_details(doc, transformation_resources):
 
 def add_assay_details(doc, transformation_resources):
     if 'dataset_type' in doc:
-        assay_details = _get_assay_details(doc, transformation_resources)
+        uuid = doc.get('uuid')
+        soft_assay_url = transformation_resources.get(
+            'ingest_api_soft_assay_url')
+        token = transformation_resources.get('token')
+
+        request_url, headers = f'{soft_assay_url}/{uuid}', {
+            'Authorization': f'Bearer {token}'}
+
+        assay_details = _get_assay_details(
+            doc, uuid=uuid, request_url=request_url, headers=headers)
+
         # Preserve the previous shape of mapped_data_types.
         doc['mapped_data_types'] = [assay_details.get('description')]
         doc['vitessce-hints'] = assay_details.get('vitessce-hints')
+
+        def get_assay_type_for_viz(doc):
+            return requests.get(request_url, headers=headers).json()
+
+        doc['visualization'] = has_visualization(
+            doc, get_assay_type_for_viz)
