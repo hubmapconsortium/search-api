04f6858a4c105529076e74af154cb55eeaad6ae3
John Conroy <john-conroy@users.noreply.github.com>
2023-12-19 18:09:23 -0500

    Update to call soft assay once

--- src/hubmap_translation/addl_index_transformations/portal/add_assay_details.py
+++ src/hubmap_translation/addl_index_transformations/portal/add_assay_details.py
@@ -8,41 +8,35 @@ logging.basicConfig(format='[%(asctime)s] %(levelname)s in %(module)s: %(message
 logger = logging.getLogger(__name__)
 
 
-def _get_assay_details(doc, uuid, request_url, headers):
+def _get_assay_details(doc, transformation_resources):
+    soft_assay_url = transformation_resources.get('ingest_api_soft_assay_url')
+    token = transformation_resources.get('token')
+    uuid = doc.get('uuid')
     dataset_type = doc.get('dataset_type')
 
     try:
-        response = requests.get(request_url, headers=headers)
-        response.raise_for_status()
+        response = requests.get(
+            f'{soft_assay_url}/{uuid}', headers={'Authorization': f'Bearer {token}'})
     except requests.exceptions.HTTPError as e:
         logger.error(e.response.text)
         raise
     json = response.json()
     if not json:
         logger.info(f"No soft assay information returned for dataset ${uuid}.")
-        return {'description': dataset_type, 'vitessce-hints': []}
+        return {'assaytype': 'unknown', 'description': dataset_type, 'vitessce-hints': ['unknown-assay']}
     return json
 
 
 def add_assay_details(doc, transformation_resources):
     if 'dataset_type' in doc:
-        uuid = doc.get('uuid')
-        soft_assay_url = transformation_resources.get(
-            'ingest_api_soft_assay_url')
-        token = transformation_resources.get('token')
-
-        request_url, headers = f'{soft_assay_url}/{uuid}', {
-            'Authorization': f'Bearer {token}'}
-
-        assay_details = _get_assay_details(
-            doc, uuid=uuid, request_url=request_url, headers=headers)
+        assay_details = _get_assay_details(doc, transformation_resources)
 
         # Preserve the previous shape of mapped_data_types.
         doc['mapped_data_types'] = [assay_details.get('description')]
         doc['vitessce-hints'] = assay_details.get('vitessce-hints')
 
         def get_assay_type_for_viz(doc):
-            return requests.get(request_url, headers=headers).json()
+            return assay_details
 
         doc['visualization'] = has_visualization(
             doc, get_assay_type_for_viz)
