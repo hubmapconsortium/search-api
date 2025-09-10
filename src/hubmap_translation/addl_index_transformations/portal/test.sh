902595ff40c58c8ba684dfbb4c647ef0b2ac7166
yuanzhou <yuanzhou19@gmail.com>
2025-09-09 23:37:48 -0400

    Update test.sh

--- src/hubmap_translation/addl_index_transformations/portal/test.sh
+++ src/hubmap_translation/addl_index_transformations/portal/test.sh
@@ -3,12 +3,11 @@ set -o errexit
 
 cd `dirname $0`
 
-start portal/flake8
+
 flake8 \
   || die "Try: autopep8 --in-place --aggressive -r $PWD"
-end portal/flake8
 
-start portal/doctests
+
 cd ../../../
 for F in hubmap_translation/addl_index_transformations/portal/*.py; do
   CMD="python -m doctest -o REPORT_NDIFF $F"
@@ -16,10 +15,10 @@ for F in hubmap_translation/addl_index_transformations/portal/*.py; do
   eval $CMD
 done
 cd -
-end portal/doctests
 
-start portal/pytest
+
 cd ../../../
 PYTHONPATH="src:$PYTHONPATH" pytest -vv  --log-cli-level WARN
 cd -
-end portal/pytest
+
+
