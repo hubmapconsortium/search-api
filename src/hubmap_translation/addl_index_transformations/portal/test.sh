81a5d97f4c45d25071a768bfb8bc6f982542f657
DerekFurstPitt <drf57@pitt.edu>
2025-09-15 02:09:19 -0400

    removed outdated references used for travis in test.sh. removed test-utils.sh updated dev requirements for flake and pytest

--- src/hubmap_translation/addl_index_transformations/portal/test.sh
+++ src/hubmap_translation/addl_index_transformations/portal/test.sh
@@ -5,12 +5,7 @@ set -o errexit
 
 cd `dirname $0`
 
-start portal/flake8
-flake8 \
-  || die "Try: autopep8 --in-place --aggressive -r $PWD"
-end portal/flake8
-
-start portal/doctests
+flake8 
 cd ../../../
 for F in hubmap_translation/addl_index_transformations/portal/*.py; do
   CMD="python -m doctest -o REPORT_NDIFF $F"
@@ -18,10 +13,6 @@ for F in hubmap_translation/addl_index_transformations/portal/*.py; do
   eval $CMD
 done
 cd -
-end portal/doctests
-
-start portal/pytest
 cd ../../../
 PYTHONPATH="src:$PYTHONPATH" pytest -vv  --log-cli-level WARN
 cd -
-end portal/pytest
